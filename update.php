bc7c270bf26d578247ac9af64730f17900b9d2f7
Ian Murphy <immur.50ae@gmail.com>
2016-05-27 18:50:45 -0700

    dafl;sdkfn

--- update.php
+++ update.php
@@ -4,7 +4,7 @@ $json = json_decode($_POST["payload"]);
 ob_start();
 var_dump($_POST);
 $post = ob_get_clean();
-error_log($json["commits"]["message"]);
+error_log($json->{"commits"}->{"message"});
 #kill me, dsakjgl;fkns;lgn 
 
 
