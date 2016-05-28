5144c06bcb7f5fbd536316dcbf408becadd46ddd
Ian Murphy <immur.50ae@gmail.com>
2016-05-27 18:42:21 -0700

    dafl;sdkfn

--- update.php
+++ update.php
@@ -1,12 +1,14 @@
 <?php
 
 $json = json_decode($_POST["payload"]);
-
+ob_start();
+var_dump($_POST);
+$post = ob_get_clean();
 $handle = fopen("json.txt", "w");
 fwrite($handle, $json);
 fclose($handle);
 $handle = fopen("POST.txt", "w");
-fwrite($handle, $_POST);
+fwrite($handle, $post);
 fclose($handle);
 #kill me, dsakjgl;fkns;lgn
 
