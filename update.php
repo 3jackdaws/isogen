1df2723c42ae0edfb9a47dd466c6dd9f6e910c4f
Ian Murphy <immur.50ae@gmail.com>
2016-05-27 18:36:37 -0700

    php pls

--- update.php
+++ update.php
@@ -1,11 +1,12 @@
 <?php
 
 $json = json_decode($_POST["payload"]);
-ob_start();
-var_dump($json);
-$result = ob_get_clean();
-$handle = fopen("output.txt", "w");
+
+$handle = fopen("json.txt", "w");
 fwrite($handle, $json);
+fclose($handle);
+$handle = fopen("POST.txt", "w");
+fwrite($handle, $_POST);
 fclose($handle);
 #kill me, dsakjgl;fkns;lgn
 
