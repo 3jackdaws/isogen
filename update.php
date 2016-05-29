ed5c042489538177fb881019e8085096ec428f3c
Ian Murphy <immur.50ae@gmail.com>
2016-05-29 12:19:59 -0700

    Added exec commands to update. KRONOS: 'update'

--- update.php
+++ update.php
@@ -1,6 +1,6 @@
 <?php
 $handle = fopen("json.txt", "r");
-echo "opening json<br>";
+//echo "opening json<br>";
 $string = fread($handle, filesize("json.txt"));
 //var_dump($string);
 $json = json_decode($string);
@@ -11,18 +11,27 @@ foreach ($json->{"commits"} as $commit) {
 	$messages[] = $commit->{"message"};
 
 }
-var_dump($messages);
+//var_dump($messages);
 
 foreach ($messages as $message) {
 	if(preg_match("/(?<=KRONOS:)[ ]*'.+'/", $message, $match))
 	{
-		preg_match_all("/[^,.]+/", $match, $commands);
-		foreach ($commands as $cmd) {
-			echo $cmd . "\n";
+		preg_match_all("/[^,^'.]+/", $match[0], $commands);
+		//var_dump($commands);
+		foreach ($commands[0] as $cmd) {
+			runCommand($cmd);
+			echo $cmd . "<br>";
 		}
 	}
 }
 
-#kill me, dsakjgl;fkns;lgn 
+
+function runCommand($cmd){
+	if(strtolower($cmd) == "update")
+	{
+		exec("/var/www/sites/isogen/updatesite.sh");
+	}
+}
+
 
 
