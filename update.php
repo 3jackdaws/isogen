1327ef1032b0dd44831b23d098a8e5ad04dbef93
Ian Murphy <immur.50ae@gmail.com>
2016-05-29 11:24:54 -0700

    Still testing update. KRONOS:[UPDATE, TEST]

--- update.php
+++ update.php
@@ -1,10 +1,28 @@
 <?php
+$handle = fopen("json.txt", "r");
+echo "opening json<br>";
+$string = fread($handle, filesize("json.txt"));
+//var_dump($string);
+$json = json_decode($string);
+//var_dump($json);
+echo "\n\n\n\n";
+//var_dump($json);
+foreach ($json->{"commits"} as $commit) {
+	$messages[] = $commit->{"message"};
 
-$json = json_decode($_POST["payload"]);
-ob_start();
-var_dump($_POST);
-$post = ob_get_clean();
-error_log($json->{"commits"}->{"message"});
+}
+var_dump($messages);
+
+foreach ($messages as $message) {
+	if(preg_match("/(?<=KRONOS:)[ ]*'.+'/", $message, $match))
+	{
+		preg_match_all("/[^,.]+/", $match, $commands);
+		foreach ($commands as $cmd) {
+			echo $cmd . "\n";
+		}
+	}
+}
+
 #kill me, dsakjgl;fkns;lgn 
 
 
