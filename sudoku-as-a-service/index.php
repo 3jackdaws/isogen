318583bf27123571cf0fcc79f1518a6bb3afd4ed
Ian Murphy <ian@Apate.local>
2016-06-13 15:35:29 -0700

    Redid directory correctly

--- sudoku-as-a-service/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ian
- * Date: 5/25/16
- * Time: 8:08 PM
- */
-
-$gameboard
--- updatesite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-date >> ../isogen.txt
-git reset fetch --all
-git reset --hard origin/master >> ../isogen.txt
-echo "--------------------" >> ../isogen.txt
