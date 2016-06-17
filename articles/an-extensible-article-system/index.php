2417377706cdf1c9774c55cd535883cace34aca6
Ian Murphy <3jackdaws@gmail.com>
2016-06-16 19:46:54 -0700

    Everything is sorta dynamic now.  PAges are constructed by a Page class now.

--- articles/an-extensible-article-system/index.php
+++ articles/an-extensible-article-system/index.php
@@ -2,7 +2,7 @@
 $basepath = realpath($_SERVER['DOCUMENT_ROOT']);
 include_once($basepath . '/assets/php/Page.php');
 
-$page = new \Page\ArticlePage(basename(__DIR__));
+$page = new ArticlePage(basename(__DIR__));
 $page->createPage();
 ?>
 
--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 $basepath = realpath($_SERVER['DOCUMENT_ROOT']);
 include_once($basepath . '/assets/php/Page.php');
 
-$page = new \Page\MainPage();
+$page = new MainPage();
 $page->createPage();
 ?>
-<script>
-    console.log(<?=$_SERVER['HTTP_IF_MODIFIED_SINCE']?>);
-</script>
+
